a19888610a2ba317a4d255f6c82acf46437c7d94
Solomon Kim <solomonkim98@gmail.com>
2025-02-14 15:34:17 -0500

    Update home view add animation

--- TAMS Inventory/HomeView.swift
+++ TAMS Inventory/HomeView.swift
@@ -11,78 +11,83 @@ struct HomeView: View {
   @State private var showLoginSheet: Bool = false
   @State private var showActivateSheet: Bool = false
   @State private var showProgressBar: Bool = false
+  @State private var animateGradient = false
   
   var body: some View {
     GeometryReader { geometry in
-      VStack {
-        VStack {
-          Image("Header")
-            .resizable()
-            .frame(width: 335, height: 110)
-          Divider()
-            .padding(22)
-        }
-        .padding(.top, 125)
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+      ZStack {
+        // Gradient Background
+        AnimatedLavaLampGradient()
+          .edgesIgnoringSafeArea(.all)
         
-        VStack(spacing: 54) {
-          Button {
-            // Go to Login view
-          } label: {
-            HStack {
-              Text("Login")
-              Image(systemName: "arrow.right")
-            }
-            .frame(width: UIScreen.main.bounds.width - 100, height: 48)
+        VStack {
+          VStack {
+            Image("Header")
+              .resizable()
+              .frame(width: 335, height: 110)
+            Divider()
+              .padding(22)
           }
+          .padding(.top, 125)
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
           
-          Button(action: {
-            showActivateSheet.toggle()
-          }, label: {
-            HStack {
-              Text("Activate")
-              Image(systemName: "arrow.right")
+          VStack(spacing: 54) {
+            Button {
+              // Go to Login view
+            } label: {
+              HStack {
+                Text("Login")
+                Image(systemName: "arrow.right")
+              }
+              .frame(width: UIScreen.main.bounds.width - 100, height: 48)
             }
-            .frame(width: UIScreen.main.bounds.width - 100, height: 48)
-          })
-          .sheet(isPresented: $showActivateSheet, content: {
-            ActivateView()
-          })
-        }
-        .font(.title3)
-        .buttonStyle(.borderedProminent)
-        .foregroundColor(.white)
-        .padding(.top, 22)
-        .padding(.bottom, 67)
-        
-        VStack {
-          Image("Management")
-        }
-        .padding(.top, 7)
-        .padding(.bottom, 7)
-        
-        VStack {
-          Text("Version \(AppConfig().version)")
-            .fontWeight(.thin)
-        }
-        .padding(.bottom, 14)
-        
-        VStack(spacing: 14) {
-          Button("Contact Us") {
+            
+            Button(action: {
+              showActivateSheet.toggle()
+            }, label: {
+              HStack {
+                Text("Activate")
+                Image(systemName: "arrow.right")
+              }
+              .frame(width: UIScreen.main.bounds.width - 100, height: 48)
+            })
+            .sheet(isPresented: $showActivateSheet, content: {
+              ActivateView()
+            })
           }
-          Button("SOS") {
+          .font(.title3)
+          .buttonStyle(.borderedProminent)
+          .foregroundColor(.white)
+          .padding(.top, 22)
+          .padding(.bottom, 67)
+          
+          VStack {
+            Image("Management")
           }
-          //.background(Color("Solo Red")) // Not fully red because of Bordered Prominent Button Style
+          .padding(.top, 7)
+          .padding(.bottom, 7)
+          
+          VStack {
+            Text("Version \(AppConfig().version)")
+              .fontWeight(.thin)
+          }
+          .padding(.bottom, 14)
+          
+          VStack(spacing: 14) {
+            Button("Contact Us") { }
+            Button("SOS") { }
+          }
+          .buttonStyle(.borderedProminent)
+          .padding(.bottom, 100)
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
         }
-        .buttonStyle(.borderedProminent)
-        .padding(.bottom, 100)
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .frame(width: geometry.size.width, height: geometry.size.height)
+        .scaleEffect(0.9)
       }
-      .frame(width: geometry.size.width, height: geometry.size.height)
-      .scaleEffect(0.9)
     }
   }
 }
+
 
 #Preview {
   HomeView()
