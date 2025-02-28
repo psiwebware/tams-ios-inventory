f641e2ec37b83f04e59764741d689bffac7f4af8
Solomon Kim <solomonkim98@gmail.com>
2025-02-28 12:29:42 -0500

    View model initial

--- TAMS Inventory/ActivateViewModel.swift
+++ TAMS Inventory/ActivateViewModel.swift
@@ -2,7 +2,34 @@
 //  ActivateViewModel.swift
 //  TAMS Inventory
 //
-//  Created by Solomon Kim on 2/18/25.
+//  Created by Solomon Kim on 2/28/25.
 //
 
 import Foundation
+
+class ActivateViewModel: ObservableObject {
+  //@Published var activateModel = ActivateModel()
+  //@Published var userModel: [UserModel] = []
+  //@Published var inventoryModel: [InventoryModel] = []
+  
+  @Published var client: String = ""
+  @Published var site: String = ""
+  @Published var systemType: String = ""
+  @Published var systemTypeOptions = ["FAMS", "JAMS"]
+  @Published var superUsername: String = ""
+  @Published var superPassword: String = ""
+  @Published var username: String = ""
+  @Published var password: String = ""
+  
+  @Published var showSheet: Bool = false
+  @Published var showPassword: Bool = false
+  
+  @Published var showAlert: Bool = false
+  @Published var alertType: String = ""
+  @Published var alertTitle: String = ""
+  @Published var alertMessage: String = ""
+  
+  @Published var isLoading: Bool = false
+  @Published var isButtonDisabled: Bool = false
+}
+
