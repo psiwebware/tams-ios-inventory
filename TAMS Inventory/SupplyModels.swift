c0ddeb040d2778ce9007c51a6f850dd53d6436ec
Solomon Kim <solomonkim98@gmail.com>
2025-03-03 11:16:01 -0500

    Type and Supply models

--- TAMS Inventory/SupplyModels.swift
+++ TAMS Inventory/SupplyModels.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Supply: Identifiable {
+  var id: String
+  var client: String
+  var site: String
+  var systemType: String
+  var autoKey: String
+  var partId: String
+  var type: String
+  var subType: String
+  var vendor: String
+  var onHand: String
+  
+  init(id: String, client: String, site: String, systemType: String, autoKey: String, partId: String, type: String, subType: String, vendor: String, onHand: String) {
+    self.id = id
+    self.client = client
+    self.site = site
+    self.systemType = systemType
+    self.autoKey = autoKey
+    self.partId = partId
+    self.type = type
+    self.subType = subType
+    self.vendor = vendor
+    self.onHand = onHand
+  }
+}
+
+@Model
+class SupplyAdjust: Identifiable {
+  var id: String
+  var client: String
+  var site: String
+  var systemType: String
+  var autoKey: String
+  var partId: String
+  var type: String
+  var typeKey: String
+  var building: String
+  var floor: String
+  var area: String
+  var updatedDate: String
+  
+  init(id: String, client: String, site: String, systemType: String, autoKey: String, partId: String, type: String, typeKey: String, building: String, floor: String, area: String, updatedDate: String) {
+    self.id = id
+    self.client = client
+    self.site = site
+    self.systemType = systemType
+    self.autoKey = autoKey
+    self.partId = partId
+    self.type = type
+    self.typeKey = typeKey
+    self.building = building
+    self.floor = floor
+    self.area = area
+    self.updatedDate = updatedDate
+  }
+}
--- TAMS Inventory/TypeModel.swift
+++ TAMS Inventory/TypeModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Types: Identifiable {
+  var id: String
+  var client: String
+  var site: String
+  var type: String
+  var subType: String
+  var subSubType: String
+  var udf1: String
+  
+  init(id: String, client: String, site: String, type: String, subType: String, subSubType: String, udf1: String) {
+    self.id = id
+    self.client = client
+    self.site = site
+    self.type = type
+    self.subType = subType
+    self.subSubType = subSubType
+    self.udf1 = udf1
+  }
+}
